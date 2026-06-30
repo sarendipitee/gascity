546c8482b85ac73f07197452551259fdeefef800
duncan4123 <duncan4123@users.noreply.github.com>
2026-06-30 21:23:09 +1000

    scrub loader env for DoltLite init
    (cherry picked from commit 09a373d937112bd2d6a9d9298d1de87d68b0a8e5)

--- cmd/gc/session_lifecycle_parallel.go
+++ cmd/gc/session_lifecycle_parallel.go
@@ -1030,6 +1030,9 @@ func buildPreparedStartWithWorkDirResolver(
 	if triggerEnv := sessionTriggerBeadEnv(session); len(triggerEnv) > 0 {
 		agentCfg.Env = mergeEnv(agentCfg.Env, triggerEnv)
 	}
+	if cityUsesDoltliteBackend(cfg) {
+		agentCfg.Env = mergeEnv(agentCfg.Env, doltliteLoaderEnvScrub())
+	}
 	agentCfg = runtime.SyncWorkDirEnv(agentCfg)
 	return &preparedStart{
 		candidate:     candidate,
