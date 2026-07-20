0941d0c6e25cdc0529378e0e9fc24b7698a84afc
Saren <saren@dumstruck.com>
2026-07-20 00:32:48 +0000

    fix: restore integration after upstream rebase

--- cmd/gc/city_runtime_test.go
+++ cmd/gc/city_runtime_test.go
@@ -3866,7 +3866,9 @@ func TestCityRuntimeTickRunsOnDeathWithCanonicalRigEnv(t *testing.T) {
 		},
 	}
 
-	cr.reconcilePoolDeaths(&prevPoolRunning)
+	dirty := &atomic.Bool{}
+	var lastProviderName string
+	cr.tick(context.Background(), dirty, &lastProviderName, cityPath, &prevPoolRunning, "test")
 
 	data, err := os.ReadFile(outFile)
 	if err != nil {
@@ -3910,7 +3912,9 @@ func TestCityRuntimeTickSkipsOnDeathWhenSessionListingIsPartial(t *testing.T) {
 		},
 	}
 
-	cr.reconcilePoolDeaths(&prevPoolRunning)
+	dirty := &atomic.Bool{}
+	var lastProviderName string
+	cr.tick(context.Background(), dirty, &lastProviderName, cityPath, &prevPoolRunning, "test")
 
 	if _, err := os.Stat(outFile); !errors.Is(err, os.ErrNotExist) {
 		t.Fatalf("on_death output err = %v, want no hook execution", err)
